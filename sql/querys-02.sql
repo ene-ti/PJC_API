755ed73ce390311f6be9c5508daeb1c18d29f68f
Nelsimar <nelsimargoncalves1@gmail.com>
2021-01-07 23:22:17 -0300

    Criação de DB e script de insert

---
+++ sql/querys-02.sql
@@ -0,0 +1,21 @@
+-- Insert de Artistas:
+INSERT INTO DB_PJC.ARTISTA (art_id, art_nome) VALUES (1, 'Serj tankian');
+INSERT INTO DB_PJC.ARTISTA (art_id, art_nome) VALUES (2, 'Mike Shinoda');
+INSERT INTO DB_PJC.ARTISTA (art_id, art_nome) VALUES (3, 'Michel Teló');
+INSERT INTO DB_PJC.ARTISTA (art_id, art_nome) VALUES (4, 'Guns N´Roses');
+
+-- Insert de Albuns:
+INSERT INTO DB_PJC.ALBUM (alb_id, alb_nome, art_id) VALUES (1, "Harakiri", 1);
+INSERT INTO DB_PJC.ALBUM (alb_id, alb_nome, art_id) VALUES (2, "Black Blooms", 1);
+INSERT INTO DB_PJC.ALBUM (alb_id, alb_nome, art_id) VALUES (3, "The Rough Dog", 1);
+INSERT INTO DB_PJC.ALBUM (alb_id, alb_nome, art_id) VALUES (4, "The Rising Tied", 2);
+INSERT INTO DB_PJC.ALBUM (alb_id, alb_nome, art_id) VALUES (5, "Post Traumatic", 2);
+INSERT INTO DB_PJC.ALBUM (alb_id, alb_nome, art_id) VALUES (6, "Post Traumatic EP", 2);
+INSERT INTO DB_PJC.ALBUM (alb_id, alb_nome, art_id) VALUES (7, "Where'd You Go", 2);
+INSERT INTO DB_PJC.ALBUM (alb_id, alb_nome, art_id) VALUES (8, "Bem Sertanejo", 3);
+INSERT INTO DB_PJC.ALBUM (alb_id, alb_nome, art_id) VALUES (9, "Bem Sertanejo - O Show (Ao Vivo)", 3); 
+INSERT INTO DB_PJC.ALBUM (alb_id, alb_nome, art_id) VALUES (10, "Bem Sertanejo - (1ª Temporada) - EP", 3);
+INSERT INTO DB_PJC.ALBUM (alb_id, alb_nome, art_id) VALUES (11, "Use Your IIIlusion I", 4);
+INSERT INTO DB_PJC.ALBUM (alb_id, alb_nome, art_id) VALUES (12, "Use Your IIIlusion II", 4); 
+INSERT INTO DB_PJC.ALBUM (alb_id, alb_nome, art_id) VALUES (13, "Greatest Hits", 4);
+
